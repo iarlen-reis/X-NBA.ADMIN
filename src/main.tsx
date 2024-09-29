a4523dad2dbbf3dcb476c29ff7705c8caaf0a59d
Iarlen <iarlensr@hotmail.com>
2024-09-28 22:26:02 -0300

    feat: add routes to team pages.

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,22 @@ import Layout from "./screens/layout";
 import React from "react";
 import App from "./App";
 import "./index.css";
+import Teams from "./screens/teams";
+import NewTeam from "./screens/new-team";
+import TeamDetail from "./screens/team-detail";
+import TeamEdit from "./screens/team-edit";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ path: "/", element: <App /> }],
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/teams", element: <Teams /> },
+      { path: "/teams/new", element: <NewTeam /> },
+      { path: "/teams/:id", element: <TeamDetail /> },
+      { path: "/teams/:id/edit", element: <TeamEdit /> },
+    ],
   },
 ]);
 
