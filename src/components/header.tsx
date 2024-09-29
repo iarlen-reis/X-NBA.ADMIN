39fcd7e03c10c9c13a8348c63d9f12dcd4036be1
Iarlen <iarlensr@hotmail.com>
2024-09-28 22:25:07 -0300

    feat: add some links on header.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,13 +3,26 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="w-full py-2">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex items-center justify-between">
         <Link
           to="/"
           className="text-xl font-bold transition-opacity hover:opacity-85"
         >
           X-NBA.ADMIN
         </Link>
+        <nav>
+          <ul className="flex items-center gap-3">
+            <li className="transition-opacity hover:opacity-85">
+              <Link to="/teams">Times</Link>
+            </li>
+            <li className="transition-opacity hover:opacity-85">
+              <Link to="/players">Jogadores</Link>
+            </li>
+            <li className="transition-opacity hover:opacity-85">
+              <Link to="/matches">Partidas</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
